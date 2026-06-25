714101526eedf6e7b38545a849afb7794f49f70d
isaaespriella <elenaespri@gmaill.com>
2026-06-24 23:13:05 -0600

    Cambios

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,13 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+@Get()
+  health() {
+    return {
+      status: 'ok',
+    };
+  }
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
